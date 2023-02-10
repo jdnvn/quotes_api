6c096e963cc96a36cb38695b3ea091c012c47072
Joey Donovan <jsphdnvn@gmail.com>
2023-02-09 20:11:10 -0500

    add books update and delete operations

---
+++ app/services/books/update.rb
@@ -0,0 +1,27 @@
+module Books
+  class Update < Base
+    attr_reader :book, :attributes
+
+    def initialize(book:, attributes:)
+      @book = book
+      @attributes = attributes
+    end
+
+    def call
+      update_book
+      success(book)
+    rescue StandardError => e
+      Rails.logger.error(e)
+      failure(error: e)
+    end
+
+    private
+
+    def update_highlight
+      book.update(
+        title: attributes[:title],
+        author: attributes[:author]
+      )
+    end
+  end
+end
