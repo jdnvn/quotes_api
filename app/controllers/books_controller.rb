a13af3e38adcbfdd01b74fd954f04f14e857afec
Joey Donovan <jsphdnvn@gmail.com>
2023-02-11 01:18:47 -0500

    fix some stuff

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,8 @@
 class BooksController < ApplicationController
   def index
     # TODO: filter books
-    render json: Book.all, status: :ok
+    books = Book.order('books.created_at DESC')
+    render json: books, status: :ok
   end
 
   def show
@@ -14,6 +15,25 @@ class BooksController < ApplicationController
     end
   end
 
+  def update
+    book = Book.find_by(id: params[:id])
+    return render json: "No book with that id", status: :not_found unless book
+
+    result = Books::Update.new(book: book, attributes: params).call
+
+    if result.success?
+      render json: result.value, status: :ok
+    else
+      render json: result.error, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    Book.find_by(id: params[:id]).destroy!
+
+    render status: :no_content
+  end
+
   def create
     result = Books::Create.new(attributes: params).call
 
@@ -23,23 +43,4 @@ class BooksController < ApplicationController
       render json: { error: result.error }, status: :unprocessable_entity
     end
   end
-end
-
-def update
-  book = Book.find_by(id: params.dig(:params, :id))
-  return render json: "No book with that id", status: :not_found unless book
-
-  result = Books::Update.new(book: book, attributes: params["params"]).call
-
-  if result.success?
-    render json: result.value, status: :ok
-  else
-    render json: result.error, status: :unprocessable_entity
-  end
-end
-
-def destroy
-  Book.find_by(id: params[:id]).destroy!
-
-  render status: :no_content
 end
